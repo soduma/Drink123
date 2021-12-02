c0e0cc8af95b9807caa51c56ca8cecf6c1096fb3
soduma <soduma@naver.com>
2021-12-02 12:41:56 +0900

    xib nono

--- Drink/ViewController.swift
+++ Drink/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addAlertButton(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
