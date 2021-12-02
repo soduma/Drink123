b477952cd2d23cbbee13a996125e8903c224d3b0
soduma <soduma@naver.com>
2021-12-02 15:33:22 +0900

    notification trigger

--- Drink/UNNotificationCenter.swift
+++ Drink/UNNotificationCenter.swift
@@ -16,5 +16,7 @@ extension UNUserNotificationCenter {
         content.sound = .default
         content.badge = 1
         
+        let component = Calendar.current.dateComponents([.hour, .minute], from: alert.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: alert.isOn)
     }
 }
